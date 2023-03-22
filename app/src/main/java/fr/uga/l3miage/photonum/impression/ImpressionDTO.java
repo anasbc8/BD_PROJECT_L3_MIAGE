f115290b03abbd9847d3bed7d3e429453c96d1d0
Boukha <boukha@etu-173-99.vpn.u-ga.fr>
2023-03-22 12:17:02 +0100

    commit

--- app/src/main/java/fr/uga/l3miage/photonum/impression/ImpressionDTO.java
+++ app/src/main/java/fr/uga/l3miage/photonum/impression/ImpressionDTO.java
@@ -1,7 +1,30 @@
 package fr.uga.l3miage.photonum.impression;
 
-import jakarta.validation.constraints.NotBlank;
+import fr.uga.l3miage.photonum.data.domain.Catalogue;
+import fr.uga.l3miage.photonum.data.domain.Impression;
+import jakarta.validation.constraints.NotNull;
 
 public record ImpressionDTO(
+        @NotNull String id,
+        double price,
+        @NotNull ArticleDTO article,
+        @NotNull Catalogue reference
 ) {
+    public static ImpressionDTO fromEntity(Impression impression) {
+        return new ImpressionDTO(
+                impression.getId(),
+                impression.getPrice(),
+                ArticleDTO.fromEntity(impression.getArticle()),
+                impression.getReference()
+        );
+    }
+
+    public Impression toEntity() {
+        Impression impression = new Impression();
+        impression.setId(id);
+        impression.setPrice(price);
+        impression.setArticle(article.toEntity());
+        impression.setReference(reference);
+        return impression;
+    }
 }
