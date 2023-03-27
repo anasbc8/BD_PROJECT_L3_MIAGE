baf72245e4be65e5e697ceecdcb2d40e081f711e
mouad.el-kbabty@etu.uni-grenoble-alpes.fr <mouaadelkbabty@gmail.com>
2023-03-27 15:39:32 +0200

    Update

--- data/src/main/java/fr/uga/l3miage/photonum/data/domain/Image.java
+++ data/src/main/java/fr/uga/l3miage/photonum/data/domain/Image.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
-public abstract class Image {
+public class Image {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
