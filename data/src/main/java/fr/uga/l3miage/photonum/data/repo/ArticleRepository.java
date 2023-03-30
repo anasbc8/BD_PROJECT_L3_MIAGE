255141d3af44501a504250e11c90f61b24e79428
l3miage-essouaik <hirohito595@gmail.com>
2023-03-30 20:06:04 +0200

    ArticleRepo & ArticleRepoTest

--- data/src/main/java/fr/uga/l3miage/photonum/data/repo/ArticleRepository.java
+++ data/src/main/java/fr/uga/l3miage/photonum/data/repo/ArticleRepository.java
@@ -4,6 +4,7 @@ import fr.uga.l3miage.photonum.data.domain.Article;
 import fr.uga.l3miage.photonum.data.domain.Commande;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -26,12 +27,24 @@ public class ArticleRepository implements CRUDRepository<String, Article> {
 
     @Override
     public void delete(Article article) {
-    entityManager.remove(article);
+        entityManager.remove(article);
     }
 
     @Override
     public List<Article> all() {
-        String query = "select a FROM Article order by a.quantity" ;
+        String query = "select a FROM Article a order by a.quantity" ;
         return entityManager.createQuery(query, Article.class).getResultList();
     }
+
+    public void update(Article article) {
+        Query query = entityManager.createQuery(
+                "UPDATE Article a SET a.quantity = :quantity, a.prixTotal = :prixTotal, a.impression = :impression WHERE a.id = :id"
+        );
+        query.setParameter("quantity", article.getQuantity());
+        query.setParameter("prixTotal", article.getPrixTotal());
+        query.setParameter("impression", article.getImpression());
+        query.setParameter("id", article.getId());
+        query.executeUpdate();
+    }
+
 }
