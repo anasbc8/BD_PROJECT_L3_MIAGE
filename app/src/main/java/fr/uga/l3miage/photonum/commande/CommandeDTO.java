18e3bece81c7f7b23b7e609819786088e499f8f9
mouad.el-kbabty@etu.uni-grenoble-alpes.fr <mouaadelkbabty@gmail.com>
2023-04-01 17:39:13 +0200

    Commande Mapper/controller/dto

---
+++ app/src/main/java/fr/uga/l3miage/photonum/commande/CommandeDTO.java
@@ -0,0 +1,27 @@
+package fr.uga.l3miage.photonum.commande;
+
+import fr.uga.l3miage.photonum.data.domain.Article;
+import fr.uga.l3miage.photonum.data.domain.Client;
+import fr.uga.l3miage.photonum.data.domain.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Date;
+import java.util.Set;
+
+public record CommandeDTO (
+    @NotBlank(message = "command's id  is mandatory")
+     String id,
+
+     Date createdate,
+
+     Double totalPrice,
+
+    @Enumerated(EnumType.STRING)
+     Status status,
+
+     Set<Article> articles,
+
+     Client client
+)
+{}
--- service-pub/src/main/java/fr/uga/l3miage/photonum/service/CommandeService.java
+++ service-pub/src/main/java/fr/uga/l3miage/photonum/service/CommandeService.java
@@ -3,8 +3,11 @@ import fr.uga.l3miage.photonum.data.domain.Calendrier;
 import fr.uga.l3miage.photonum.data.domain.Commande;
 import fr.uga.l3miage.photonum.service.base.BaseService;
 
+import java.util.List;
+
 public interface CommandeService extends BaseService<Commande, String> {
     Commande save(Commande commande);
     void delete(String id) throws EntityNotFoundException ;
     Commande update(Commande commande) throws EntityNotFoundException;
+    public List<Commande> list() ;
 }
