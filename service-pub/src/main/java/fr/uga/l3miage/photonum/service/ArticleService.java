1cf355794b58d315dad64ac015e249421a56ed01
mouad.el-kbabty@etu.uni-grenoble-alpes.fr <mouaadelkbabty@gmail.com>
2023-04-01 14:42:25 +0200

    ArticleService Impl/Pub

---
+++ service-pub/src/main/java/fr/uga/l3miage/photonum/service/ArticleService.java
@@ -0,0 +1,15 @@
+package fr.uga.l3miage.photonum.service;
+
+import fr.uga.l3miage.photonum.data.domain.Article;
+import fr.uga.l3miage.photonum.data.domain.Calendrier;
+import fr.uga.l3miage.photonum.service.base.BaseService;
+
+import java.util.List;
+
+public interface ArticleService extends BaseService<Article, String> {
+     Article save(Article article);
+     List<Article> list() ;
+     void delete(String id) throws EntityNotFoundException;
+     Article get(String id) throws EntityNotFoundException;
+     Article update(Article article) throws EntityNotFoundException;
+}
